26935f988610ea48306f25539a6ab2c41f8252d8
michalmonday <michalmonday17@gmail.com>
2020-06-15 17:40:27 +0100

    Updated docs

--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -1,27 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "acgops~",
-  1: "c",
-  2: "c",
-  3: "cgops~",
-  4: "a"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "files",
-  3: "functions",
-  4: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Files",
-  3: "Functions",
-  4: "Pages"
 };
 
